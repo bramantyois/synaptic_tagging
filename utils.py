cdefaeb1084d3b601faa587a9c648f7214e15c13
bramantyois <ibrahim.bramantyo@gmail.com>
2022-09-28 15:33:21 +0200

    adding report

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_trajectory(sol, keys=list(), save_image=True, filename='plot'):
+def plot_trajectory(ax, t, sol, keys=['w1', 'w2', 'w1+w2'], save_image=True, title='plot'):
+    ax.plot(t, sol, label=keys)
+    ax.set_xlabel('time(s)')
+    ax.set_ylabel('value')
+    ax.set_title(title)
+    ax.legend()
+    ax.grid()
     
-    plt.plot(sol[:,i])
     
